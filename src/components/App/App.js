be888cba270071e5a75f2667d3b059e4bd8f9952
Myron <dubtwee@gmail.com>
2019-05-09 11:42:22 -0500

    reverting master to displaying of simple creature list

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,77 +2,16 @@ import React from 'react';
 
 class App extends React.Component {
   state = {
-    newInput: {
-      name: '',
-      origin: '',
-    },
-    creaturesList: [
-      { name: 'Unicorn', origin: 'European' },
-      { name: 'Dragon', origin: 'China' },
-      { name: 'Kappa', origin: 'Japan' }
-    ],
+    creaturesList: [ 'Unicorn', 'Dragon', 'Kappa' ],
   };
 
-  // storeNewCreatureName = (event) => {
-  //   const inputValue = event.target.value;
-  //   console.log('inputValue', inputValue);
-  //   this.setState({
-  //     newInput: {
-  //       ...this.state.newInput,
-  //       name: inputValue,
-  //     }
-  //   });
-  // }
-
-  // storeNewCreatureOrigin = (event) => {
-  //   const inputValue = event.target.value;
-  //   console.log('inputValue', inputValue);
-  //   this.setState({
-  //     newInput: {
-  //       ...this.state.newInput,
-  //       origin: inputValue,
-  //     }
-  //   });
-  // }
-
-  storeNewCreature = (event) => {
-    const inputValue = event.target.value;
-    const propertyKey = event.target.getAttribute('name');
-    console.log('propertyKey', propertyKey);
-    console.log('inputValue', inputValue);
-    this.setState({
-      newInput: {
-        ...this.state.newInput,
-        [propertyKey]: inputValue,
-      }
-    });
-  }
-
-  addNewCreature = (event) => {
-    const newCreature = {
-      name: this.state.newInput.name,
-      origin: this.state.newInput.origin,
-    };
-    this.setState({
-      creaturesList: [...this.state.creaturesList, newCreature],
-      newInput: {
-        name: '',
-        origin: '',
-      }
-    })
-  }
-
   render() {
-    // const htmlCreatures = [];
-    // for (let i = 0; i < this.state.creaturesList.length; i++) {
-    //   const indvCreature = this.state.creaturesList[i]
-    //   htmlCreatures.push(<li key={i}>{indvCreature}</li>);
-    // }
-    const htmlCreatures = this.state.creaturesList.map((indvCreature, i) => {
-      return <li key={i}>{indvCreature.name} is from {indvCreature.origin}</li>;
-    })
+    const htmlCreatures = [];
+    for (let i = 0; i < this.state.creaturesList.length; i++) {
+      const indvCreature = this.state.creaturesList[i]
+      htmlCreatures.push(<li key={i}>{indvCreature}</li>);
+    }
     console.log(htmlCreatures);
-    // const inputField = <input placeholder="variable" />
 
     return (
       <div>
@@ -80,21 +19,6 @@ class App extends React.Component {
         <p>
           {JSON.stringify(this.state.creaturesList)}
         </p>
-        <input
-          type="text"
-          name="name"
-          placeholder="Creature Name"
-          value={this.state.newInput.name}
-          onChange={this.storeNewCreature}
-        />
-        <input
-          type="text"
-          name="origin"
-          placeholder="Origin"
-          value={this.state.newInput.origin}
-          onChange={this.storeNewCreature}
-        />
-        <button onClick={this.addNewCreature}>Add New Creature</button>
         <ul>
           { htmlCreatures }
         </ul>
